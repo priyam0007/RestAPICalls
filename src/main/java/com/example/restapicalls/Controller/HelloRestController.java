b1ab2be989f79577417c2fc9426577db8ada9897
Your Name <you@example.com>
2022-04-21 23:55:53 +0530

    Added: rest call to show message by name as query parameter

---
+++ src/main/java/com/example/restapicalls/Controller/HelloRestController.java
@@ -0,0 +1,21 @@
+package com.example.restapicalls.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/web")
+public class HelloRestController {
+
+    @GetMapping("/message")
+    public String message() {
+        return "Hello From BridgeLabz";
+    }
+
+    @GetMapping("/query")
+    public String sayHello(@RequestParam String name) {
+        return "Hello "+name+" From BridgeLabz";
+    }
+}
