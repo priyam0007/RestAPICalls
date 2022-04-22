d56c05b08962bd98d7ba044fa0841ca72447a641
Your Name <you@example.com>
2022-04-22 07:17:36 +0530

    Added: rest call by passing firstname as path variable and lastname…

--- src/main/java/com/example/restapicalls/Controller/HelloRestController.java
+++ src/main/java/com/example/restapicalls/Controller/HelloRestController.java
@@ -1,7 +1,11 @@
 package com.example.restapicalls.Controller;
 
+import com.example.restapicalls.Model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +27,15 @@ public class HelloRestController {
     @GetMapping("/param/{name}")
     public String sayHelloParam(@PathVariable String name) {
         return "Hello "+name+" From BridgeLabz";
+    }
+
+    @PostMapping("/post")
+    public String userData(@RequestBody User user) {
+        return "Hello "+user.getfName()+" "+user.getlName()+" from BridgeLabz";
+    }
+    @PutMapping("/put/{fName}")
+    public String sayHello(@PathVariable String fName,@RequestParam String lName){
+        return "Hello "+fName+" "+lName+ " from BridgeLabz";
     }
 }
 
